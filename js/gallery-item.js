7f536ec34268bd940b470c822354126fa2d4ea2f
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-11-11 23:40:25 +0200

    finish

---
+++ js/gallery-item.js
@@ -0,0 +1,65 @@
+export const galleryItems = [
+  {
+    preview: '/img/min-size/WH001-min.jpg',
+    original: '/img/full-size/WH001.jpg',
+    description: 'Forum Ukraine of the future 2.0. Slide-001',
+  },
+  {
+    preview: '/img/min-size/WH002-min.jpg',
+    original: '/img/full-size/WH002.jpg',
+    description: 'Forum Ukraine of the future 2.0. Slide-002',
+  },
+  {
+    preview: '/img/min-size/WH003-min.jpg',
+    original: '/img/full-size/WH003.jpg',
+    description: 'Forum Ukraine of the future 2.0. Slide-003',
+  },
+  {
+    preview: '/img/min-size/WH004-min.jpg',
+    original: '/img/full-size/WH004.jpg',
+    description: 'Forum Ukraine of the future 2.0. Slide-004',
+  },
+  {
+    preview: '/img/min-size/WH005-min.jpg',
+    original: '/img/full-size/WH005.jpg',
+    description: 'Forum Ukraine of the future 2.0. Slide-005',
+  },
+  {
+    preview: '/img/min-size/WH006-min.jpg',
+    original: '/img/full-size/WH006.jpg',
+    description: 'Forum Ukraine of the future 2.0. Slide-006',
+  },
+  {
+    preview: '/img/min-size/WH007-min.jpg',
+    original: '/img/full-size/WH007.jpg',
+    description: 'Forum Ukraine of the future 2.0. Slide-007',
+  },
+  {
+    preview: '/img/min-size/WH008-min.jpg',
+    original: '/img/full-size/WH008.jpg',
+    description: 'Forum Ukraine of the future 2.0. Slide-008',
+  },
+  {
+    preview: '/img/min-size/WH009-min.jpg',
+    original: '/img/full-size/WH009.jpg',
+    description: 'Forum Ukraine of the future 2.0. Slide-009',
+  },
+
+];
+
+for (let i = 10; i < 100; i++) {
+  const item = {
+    preview: `/img/min-size/WH0${i}-min.jpg`,
+    original: `/img/full-size/WH0${i}.jpg`,
+    description: `Forum Ukraine of the future 2.0. Slide-0${i}`,
+  }
+  galleryItems.push(item);
+}
+for (let i = 100; i < 753; i++) {
+  const item = {
+    preview: `/img/min-size/WH${i}-min.jpg`,
+    original: `/img/full-size/WH${i}.jpg`,
+    description: `Forum Ukraine of the future 2.0. Slide-${i}`,
+  }
+  galleryItems.push(item);
+}
--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,54 @@
-function myFunction(imgs) {
-    // Get the expanded image
-    var expandImg = document.getElementById("expandedImg");
-    // Get the image text
-    var imgText = document.getElementById("imgtext");
-    // Use the same src in the expanded image as the image being clicked on from the grid
-    expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandImg.parentElement.style.display = "block";
+import { galleryItems } from './gallery-item.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryCardsSet = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return `
+            <li
+              class="gallery__item"
+              style="border-radius: 36px;
+              background: transparent;
+              box-shadow: none;">
+                <a
+                  class="gallery__item"
+                  href="${original}" 
+                  style= "box-shadow: none;" style="border-radius: 36px;"
+                >
+                  <img
+                    class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}" 
+                    style="border: 1px solid transparent;
+                           border-radius: 36px;
+                           padding: 5px; 
+                           background: rgba(255, 255, 255, 0.3); box-shadow: 0px 1px 3px 0px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 2px 1px -1px rgb(0 0 0 / 12%);"
+                   />
+                   <a
+                      class="link-download link"
+                      href="${original}"
+                      download
+                    >
+                      <svg class="icon-download" width="18" height="18">
+                        <use href="./img/symbol-defs.svg#icon-download"></use>
+                        <span class="signature-download">Завантажити</span>
+                      </svg>
+                    </a>
+                </a>
+            </li>`;
+        })
+        .join('');
 }
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardsSet);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
